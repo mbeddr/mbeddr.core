e6f6c5ea7feade06d5d6dc38b7ec8ac3ef30f0fb
Marcel Matzat <marcel.matzat@googlemail.com>
2012-05-14 11:50:12 +0200

    add testfiles

---
+++ code/languages/com.mbeddr.core/tests/test.cstub.testheader/headers/implementation/SimpleImplementationFile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int a10=10;
+
+int b20=20;
+
+int c30=30;
+
+int add(int a, int b){
+	int ret;
+	ret=a+b;
+	return ret;
+}
+
+int main(void) {
+
+	int result=add(a10, b20);
+	if(result==c30){
+		printf("sum=30");
+	} else {
+		printf("sum not 30: %i", add(a10,b20));
+	}
+	return 0;
+}
