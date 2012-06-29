28949dc6ea54d83709a145456f31618354b94803
Marcel Matzat <marcel.matzat@googlemail.com>
2012-06-29 16:51:28 +0200

    add test file

---
+++ code/languages/com.mbeddr.core/tests/test.cstub.testheader/headers/implementation/SwitchTest.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void funWithSwitchCaseAndDefault(){
+	int8_t value = 10;
+	switch(value){
+		case 1:
+       			float var1;
+		   	break;
+		case 2:
+       			float var2;
+		default:
+       			float var2;
+       			float var2;
+			float var3;
+	}			
+};
+
+void funWithSwitchCaseNoDefault(){
+	int8_t value = 10;
+	switch(value){
+		case 1:
+       			float var1;
+		   	break;
+		case 2:
+       			float var2;
+	}			
+};
+
+void funWithSwitchNoCaseWithDefault(){
+	int8_t value = 10;
+	switch(value){
+		default:
+       			float var2;
+	}			
+};
