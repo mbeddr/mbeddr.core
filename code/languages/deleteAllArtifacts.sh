737f2cdd1a25f7c97432a41b15e8eeccaca00e3f
Bernd Kolb <b.kolb@kolbware.de>
2012-07-21 09:45:45 +0200

    script for deleting generated artifacts

---
+++ code/languages/deleteAllArtifacts.sh
@@ -0,0 +1,15 @@
+cd com.mbeddr.mpsutil
+ant delete-mps-generated-code
+cd ..
+
+cd com.mbeddr.core
+ant delete-mps-generated-code
+cd ..
+
+cd com.mbeddr.cc
+ant delete-mps-generated-code
+cd ..
+
+cd com.mbeddr.ext
+
+cd ..
