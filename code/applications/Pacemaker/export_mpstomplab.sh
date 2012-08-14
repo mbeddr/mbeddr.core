492ceaeda830b23cb153bb2e9c3d999499c2bf1b
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-08-14 13:16:06 +0200

    A first attempt to refactor the code into separate modules. Currently the entire project can not be built via the solution 'Pacemaker.main' because, although the code is correct, the 'Gizmo' module generates code with broken dependencies. It can only be built via the 'Heart' module that hosts the main entry point logic of the application.

--- code/applications/Pacemaker/export_mpstomplab.sh
+++ code/applications/Pacemaker/export_mpstomplab.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 
 # info region
-SPATH="src/mps/solutions/Pacemaker.main/source_gen/Pacemaker/main/Heart" 
+SPATH="src/mps/solutions/Pacemaker.main/source_gen/Pacemaker/main" 
 DPATH="src/mplab"
 
 # fix the mplab incompatibilities from the mbedder generated code and move the code into the mplab source directory
 # Known inconsistencies : 
 # [mbeddr - statemachines] void* ___args[] = {}; --> [mplab] void* ___args[];
-for file in `ls "$SPATH" | egrep '\.[c|h]$'`; do
-  sed 's/ = {};/;/g' "$SPATH/$file" |
-  sed 's/int8_t ___rb =/const int8_t ___rb =/g' > "$DPATH/$file"
+for file in `find "$SPATH" | egrep '\.[c|h]$'`; do
+  fname=$(basename $file)
+  echo "Processing file: '$fname'..."
+  cat "$file" | 
+  sed 's/ = {};/;/g' | 
+  sed 's/int8_t ___rb =/const int8_t ___rb =/g' > "$DPATH/$fname"
 done
