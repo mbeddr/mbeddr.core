12d5802e61c8ab2836f9855a5c418eaa708b9559
Markus Voelter <voelter@acm.org>
2012-11-07 18:54:13 +0100

    changed the build file for the user guide

--- documentation/build-userguide.sh
+++ documentation/build-userguide.sh
@@ -2,4 +2,8 @@ rm -rf build/user
 mkdir build
 mkdir build/user
 cd userguide
+bibtex mbeddr-userguide
 pdflatex -output-directory=../build/user mbeddr-userguide.tex
+bibtex mbeddr-userguide
+pdflatex -output-directory=../build/user mbeddr-userguide.tex
+
