848c3700107ee11f617baf6027acf3532529dd17
Domenik Pavletic <Domenik@Pavletic.com>
2013-03-08 15:00:50 +0100

    updated the rebuildLanguagesAfterCheckout script and created one for offline usgage

--- code/languages/rebuildLanguagesAfterCheckout.sh
+++ code/languages/rebuildLanguagesAfterCheckout.sh
@@ -1,31 +1,3 @@
-cd com.mbeddr.mpsutil
-ant build-languages
-cd ..
+ant -f install-plugins.xml
 
-cd com.mbeddr.spawner
-ant build-languages
-cd ..
-
-cd com.mbeddr.debugger
-ant build-languages
-cd ..
-
-cd com.mbeddr.core
-ant build-languages
-cd ..
-
-cd com.mbeddr.debugger.tests
-ant build-languages
-cd ..
-
-cd com.mbeddr.cc
-ant build-languages
-cd ..
-
-cd com.mbeddr.ext
-ant build-languages
-cd ..
-
-cd com.mbeddr.analyses
-ant build-languages
-cd ..
+./rebuildLanguagesAfterCheckoutOffline.sh
