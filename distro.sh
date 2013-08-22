c3382f2c76e9469ade799b4f8171187f22965709
Domenik Pavletic <Domenik@Pavletic.com>
2013-08-22 14:30:12 +0200

    fixed distro layout

--- distro.sh
+++ distro.sh
@@ -6,4 +6,4 @@ rm -rf build
 mkdir build
 cd artifacts
 mv com.mbeddr.tutorial.zip ../code/applications/tutorial/com.mbeddr.tutorial.zip
-zip ../build/mbeddr-%build.number%.zip com.mbeddr.allInOne.zip mbeddr-userguide.pdf com.mbeddr.tutorial.zip
+zip com.mbeddr.allInOne.zip mbeddr-userguide.pdf com.mbeddr.tutorial.zip
