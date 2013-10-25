1c34061b5bf1ac25ab6cdb0fc3db0ea5fd5c58dc
Kolja Dummann <kolja.dummann@logv.ws>
2013-10-25 14:30:51 +0200

    update rcp

--- installer/mbeddr-installer/Logger.cs
+++ installer/mbeddr-installer/Logger.cs
@@ -1,11 +1,59 @@
 using System;
+using System.IO;
 
 namespace mbeddr.installer
 {
-	public class Logger
+	public class Logger : IDisposable
 	{
-		public Logger ()
+		
+		private static Stream stream;
+		private TextWriter writer;
+
+		#region IDisposable implementation
+
+		public void Dispose ()
 		{
+			writer.Flush ();
+		}
+
+		#endregion
+
+		private Logger(Stream stream)
+		{
+			writer = new StreamWriter(stream);
+		}
+
+		public void Log(string message)
+		{
+			writer.WriteLine (message);
+		}
+
+		public void Debug(string message)
+		{
+			Log(string.Format("DEBUG::{0}", message));
+		}
+			    	    
+		public void Error(string message)
+		{
+			Log(string.Format("ERROR::{0}", message));
+		}
+
+		public static void Init(string path)
+		{
+			stream = File.Open (path, FileMode.Create);
+		}
+
+		public static void Close()
+		{
+			stream.Close ();
+		}
+
+		public static Logger Get()
+		{
+			if (stream == null)
+				throw new InvalidOperationException ("logger is not intialized");
+
+			return new Logger (stream);
 		}
 	}
 }
