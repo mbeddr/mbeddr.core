42bc42c3be2d292c15dee97b95db90d80701b868
Domenik Pavletic <Domenik@Pavletic.com>
2014-08-29 15:17:56 +0200

    submodules are now cleaned before build

--- code/languages/buildLanguages.sh
+++ code/languages/buildLanguages.sh
@@ -1,1 +1,8 @@
+# clean submodules and update/init them
+cd ../plugins/sl-all
+./clean-submodules.sh
+cd ../../..
+git submodule update --recursive --init
+cd code/languages
+# now we can build mbeddr
 ant clean-and-build
