a981e0858d2657484909f0dfefb118e3729039c2
Domenik Pavletic <Domenik@Pavletic.com>
2015-06-08 14:09:33 +0200

    shell script for invoking latex export

--- code/applications/tutorial/solutions/com.mbeddr.tutorial.documentation/build-latex.sh
+++ code/applications/tutorial/solutions/com.mbeddr.tutorial.documentation/build-latex.sh
@@ -2,21 +2,21 @@ echo ===== Rendering and Copying Visualizations
 cd _vis
 java -jar ../plantuml.jar *.puml
 cd ..
-mkdir source_gen/com/mbeddr/tutorial/documentation/tutorial/_vis
-cp _vis/*.png source_gen/com/mbeddr/tutorial/documentation/tutorial/_vis
+mkdir source_gen/com/mbeddr/tutorial/documentation/ug/_main/_vis
+cp _vis/*.png source_gen/com/mbeddr/tutorial/documentation/ug/_main/_vis
 
 echo ===== Copying Prolog
-cp -R latex-support/mbeddr-prolog.ltx source_gen/com/mbeddr/tutorial/documentation/tutorial
+cp -R latex-support/mbeddr-prolog.ltx source_gen/com/mbeddr/tutorial/documentation/ug/_main
 
 echo ===== Copying Images
-cp -R images source_gen/com/mbeddr/tutorial/documentation/tutorial
+cp -R images source_gen/com/mbeddr/tutorial/documentation/ug/_main
 
 echo ===== Running Latex
 pushd .
-cd source_gen/com/mbeddr/tutorial/documentation/tutorial
-pdflatex -interaction nonstopmode Tutorial.tex
-pdflatex -interaction nonstopmode Tutorial.tex
-open Tutorial.pdf
+cd source_gen/com/mbeddr/tutorial/documentation/ug/_main
+pdflatex -interaction nonstopmode UserGuideExport.tex
+pdflatex -interaction nonstopmode UserGuideExport.tex
+open UserGuideExport.pdf
 popd
 
 
