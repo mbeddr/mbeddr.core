a43f36d5f380a06e92154668a06fd6b979b60e51
Bernd Kolb <bernd.kolb@gmail.com>
2017-04-12 10:27:11 +0200

    utility to generate one big project

---
+++ tools/generateBigProject/src/com/mbeddr/Main.java
@@ -0,0 +1,59 @@
+package com.mbeddr;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
+
+public class Main {
+
+    private static File projectDir = new File("/Users/berndkolb/mps/_mps35/mbeddr/mbeddr.core/code/tmp");
+    private static File modulesXml = new File("/Users/berndkolb/mps/_mps35/mbeddr/mbeddr.core/code/tmp/.mps/modules.xml");
+
+
+    public static void main(String[] args) throws Exception {
+        modulesXml.getParentFile().mkdirs();
+
+
+        File langDir = new File("/Users/berndkolb/mps/_mps35/mbeddr/mbeddr.core/code/languages");
+        File pluginsDir = new File("/Users/berndkolb/mps/_mps35/mbeddr/mbeddr.core/code/plugins");
+
+        BufferedWriter w = new BufferedWriter(new FileWriter(modulesXml));
+
+        w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<project version=\"4\">\n" +
+                "  <component name=\"MPSProject\">\n" +
+                "    <projectModules>");
+        w.newLine();
+
+        traverse(langDir, w);
+        traverse(pluginsDir, w);
+
+
+        w.write("    </projectModules>\n" +
+                "  </component>\n" +
+                "</project>");
+
+        w.close();
+
+    }
+
+
+    private static void traverse(File f, BufferedWriter w) throws Exception {
+        if (f.isDirectory()) {
+            if (f.getName().equals("_spreferences")) {
+                return;
+            }
+            for (File nestedFile : f.listFiles()) {
+                traverse(nestedFile, w);
+            }
+            return;
+        }
+        String fName = f.getName();
+        if (fName.endsWith(".mpl") || fName.endsWith(".msd")) {
+            w.write("      <modulePath path=\"" + f.getAbsolutePath() + "\" />");
+            w.newLine();
+        }
+
+    }
+}
