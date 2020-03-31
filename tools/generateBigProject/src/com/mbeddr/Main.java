ebf72b783260dbcd75068453d1481c925fdee0b0
Wladimir Safonov <wladimir.safonov@itemis.de>
2020-03-31 19:49:50 +0200

    updated allProject creation tool

--- tools/generateBigProject/src/com/mbeddr/Main.java
+++ tools/generateBigProject/src/com/mbeddr/Main.java
@@ -2,22 +2,20 @@ package com.mbeddr;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileWriter;
 
 public class Main {
 
-    private static File projectDir = new File("/Users/berndkolb/mps/_mps173/mbeddr/mbeddr.core/tmp/allProject");
-    private static File modulesXml = new File("/Users/berndkolb/mps/_mps173/mbeddr/mbeddr.core/tmp/allProject/.mps/modules.xml");
+    private static File projectDir = new File("P:\\mbeddr.core-2019.3");
+    private static File modulesXml = new File(projectDir + "/tmp/allProject/.mps/modules.xml");
 
 
     public static void main(String[] args) throws Exception {
         modulesXml.getParentFile().mkdirs();
 
 
-        File langDir = new File("/Users/berndkolb/mps/_mps173/mbeddr/mbeddr.core/code/languages");
-        File pluginsDir = new File("/Users/berndkolb/mps/_mps173/mbeddr/mbeddr.core/code/plugins");
-        File testsDir = new File("/Users/berndkolb/mps/_mps173/mbeddr/mbeddr.core/code/tests");
+        File langDir = new File(projectDir + "/code/languages");
+        File testsDir = new File(projectDir + "/code/tests");
 
         BufferedWriter w = new BufferedWriter(new FileWriter(modulesXml));
 
@@ -28,7 +26,6 @@ public class Main {
         w.newLine();
 
         traverse(langDir, w);
-        traverse(pluginsDir, w);
         traverse(testsDir, w);
 
 
@@ -43,9 +40,6 @@ public class Main {
 
     private static void traverse(File f, BufferedWriter w) throws Exception {
         if (f.isDirectory()) {
-            if (f.getName().equals("_spreferences")) {
-                return;
-            }
             for (File nestedFile : f.listFiles()) {
                 traverse(nestedFile, w);
             }
@@ -53,7 +47,17 @@ public class Main {
         }
         String fName = f.getName();
         if (fName.endsWith(".mpl") || fName.endsWith(".msd") || fName.endsWith(".devkit")) {
-            w.write("      <modulePath path=\"" + f.getAbsolutePath() + "\" />");
+            String folder;
+            if (fName.contains("_spreferences")) {
+                folder = "_spreferences";
+            } else if (fName.endsWith(".mpl")) {
+                folder = "languages";
+            } else if (fName.endsWith(".msd")) {
+                folder = "solutions";
+            } else {
+                folder = "devkits";
+            }
+            w.write("      <modulePath path=\"" + f.getAbsolutePath().replace('\\', '/') + "\" folder=\"" + folder + "\" />");
             w.newLine();
         }
 
