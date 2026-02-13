b0a3da126a723aebf1e469bae4afdf0c213e1c82
Sergej Koščejev <sergej@koscejev.cz>
2026-02-13 17:42:34 +0100

    build: move versioning logic to a plugin

---
+++ build-logic/src/main/kotlin/buildlogic.versioning.gradle.kts
@@ -0,0 +1,36 @@
+import de.itemis.mps.gradle.GitBasedVersioning
+
+val ciBuild by extra(project.hasProperty("forceCI") || project.hasProperty("teamcity"))
+
+val mpsMajor: String by project
+
+val mbeddrMajor: String by extra(mpsMajor.substring(0, 4))
+val mbeddrMinor: String by extra(mpsMajor.substring(5, 6))
+
+
+val mbeddrBuild: String by extra(GitBasedVersioning.getGitBranch().let {
+    if (it == "stable" || it.matches(Regex("""maintenance-mps\d+"""))) "master"
+    else it
+})
+
+val mbeddrBuildNumber by extra(
+    if (ciBuild) {
+        val mbeddrBuildCounter = project.findProperty("mbeddrBuildCounter")?.toString()?.toInt()
+            ?: GitBasedVersioning.getGitCommitCount()
+
+        val gitBasedVersion = GitBasedVersioning.getVersion(mbeddrBuild, mbeddrMajor, mbeddrMinor, mbeddrBuildCounter)
+
+        if(mbeddrBuild == "master") {
+            gitBasedVersion
+        } else {
+            // use same logic as in all other platforms for snapshot publications
+            "$gitBasedVersion-SNAPSHOT"
+        }
+    } else {
+        "$mbeddrMajor.$mbeddrMinor-SNAPSHOT"
+    }
+)
+
+// Enable mbeddr to be assigned a different version number than mbeddr platform,
+// as well as mbeddr to be built against a specified existing mbeddr platform version
+val mbeddrPlatformBuildNumber by extra(project.findProperty("mbeddrPlatformVersion") ?: mbeddrBuildNumber)
